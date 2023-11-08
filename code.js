83f10b87c0c6810e05a1c8ebd313f92729cc3f44
Samuel Gonzalez <sgonza21@uwyo.edu>
2023-11-07 23:02:05 -0700

    Quick sort implementation done, test passes, still missing runtime analysis

--- code.js
+++ code.js
@@ -1,3 +1,19 @@
 function quicksort(array) {
-    return array;
+    const pivot = array[0];
+    const leftArr = []
+    const rightArr = []
+
+    if (array.length <= 1){
+        return array;
+    }
+
+    for (i = 1; i <array.length; i++){
+        if (array[i] < pivot) {
+            leftArr.push(array[i]);
+        } else {
+            rightArr.push(array[i])
+        }
+    }
+
+    return (quicksort(leftArr).concat(pivot)).concat(quicksort(rightArr))
 }
